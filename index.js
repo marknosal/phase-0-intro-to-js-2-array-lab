ad5c893830b855072fa6d5791bf0901011996ba9
Mark Nosal <mark.c.nosal@gmail.com>
2023-01-15 20:17:39 -0600

    Completed old lab from prework

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = () => cats.push('Ralph');
+
+const destructivelyPrependCat = () => cats.unshift('Bob');
+
+const destructivelyRemoveLastCat = () => cats.pop();
+
+const destructivelyRemoveFirstCat = () => cats.shift();
+
+const appendCat = () => [...cats, 'Broom'];
+
+const prependCat = () => ['Arnold', ...cats];
+
+const removeLastCat = () => [...cats.slice(0, -1)];
+
+const removeFirstCat = () => [...cats.slice(1)];
